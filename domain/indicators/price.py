a9cfa55c31f9906e7e5992ff184aa1191f2c15b5
SagarOnline <chopade.sagar@gmail.com>
2025-08-18 20:36:23 +0530

    added unit tests on indicators

--- domain/indicators/price.py
+++ domain/indicators/price.py
@@ -4,9 +4,5 @@ import pandas as pd
 
 @register_indicator("price")
 def indicator_price(historical_data: Union[List[Dict[str, Any]], pd.DataFrame], params: Dict[str, Any]) -> float:
-    """Get latest price."""
-    if not isinstance(historical_data, pd.DataFrame):
-        historical_data = pd.DataFrame(historical_data)
-
     price_col = params.get("price", "close")
     return float(historical_data[price_col].iloc[-1])
