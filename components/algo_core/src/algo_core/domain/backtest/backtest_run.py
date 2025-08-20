881f2db07987a0bc8f34ffebb8609b27e7494cb1
SagarOnline <chopade.sagar@gmail.com>
2025-08-21 00:51:58 +0530

    Refactored backtest engine (#17)

---
+++ components/algo_core/src/algo_core/domain/backtest/backtest_run.py
@@ -0,0 +1,38 @@
+from datetime import date
+from typing import List
+from algo_core.domain.strategy import Strategy
+from algo_core.domain.backtest.report import BacktestReport
+from algo_core.domain.trade import Trade
+
+class BacktestRun:
+
+    def __init__(self, strategy: Strategy, historical_data: List[dict], start_date: date):
+        self.strategy = strategy
+        self.historical_data = historical_data
+        self.start_date = start_date
+
+    def start(self) -> BacktestReport:
+        instrument = self.strategy.get_instrument()
+        trades = []
+        in_trade = False
+        entry_price = 0.0
+        entry_time = None
+        for i in range(0, len(self.historical_data)):
+            previous_candles = self.historical_data[:i+1]
+            if previous_candles[i]['timestamp'].date() < self.start_date:
+                continue
+            if not in_trade:
+                if self.strategy.should_enter_trade(previous_candles):
+                    entry_price = previous_candles[i]["close"]
+                    entry_time = previous_candles[i]["timestamp"]
+                    in_trade = True
+            else:
+                if self.strategy.should_exit_trade(previous_candles):
+                    exit_price = previous_candles[i]["close"]
+                    exit_time = previous_candles[i]["timestamp"]
+                    trades.append(Trade(instrument, entry_time, entry_price, exit_time, exit_price))
+                    in_trade = False
+        pnl = 0
+        for trade in trades:
+            pnl += trade.profit()
+        return BacktestReport(self.strategy.get_name(), pnl, trades)
