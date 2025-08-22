c170215508b1047cfc31163306c1d12ba9e157ec
SagarOnline <chopade.sagar@gmail.com>
2025-08-22 19:55:43 +0530

    Passed an instance of HistoricalData to BackTest

--- components/algo_core/src/algo_core/domain/backtest/engine.py
+++ components/algo_core/src/algo_core/domain/backtest/engine.py
@@ -17,6 +17,7 @@ from algo_core.domain.backtest.report import BackTestReport
 from algo_core.domain.trade import Trade
 from algo_core.domain.market import Market
 from algo_core.domain.backtest.backtest_run import BackTest
+from algo_core.src.algo_core.domain.backtest.historical_data import HistoricalData
 
 
 class BacktestEngine:
@@ -25,20 +26,20 @@ class BacktestEngine:
         self.report_repository = report_repository
 
     def start(self, strategy: Strategy, start_date: date, end_date: date) -> BackTestReport:
-        hd = self._get_historical_data(strategy, start_date, end_date)
+        hd = self._get_underlying_historical_data(strategy, start_date, end_date)
         backtest = BackTest(strategy, hd, start_date)
         report = backtest.run()
         self.report_repository.save(report)
         return report
 
-    def _get_underlying_historical_data(self, strategy, start_date, end_date):
+    def _get_underlying_historical_data(self, strategy: Strategy, start_date: date, end_date: date) -> HistoricalData:
         instrument = strategy.get_instrument()
         timeframe = Timeframe(strategy.get_timeframe())
         required_start_date = strategy.get_required_history_start_date(start_date)
         historical_data = self.historical_data_repository.get_historical_data(instrument, required_start_date, end_date, timeframe)
         return historical_data
     
-    def _get_position_historical_data(self, strategy, start_date, end_date):
+    def _get_position_historical_data(self, strategy:Strategy, start_date: date, end_date: date) -> HistoricalData:
         instrument = strategy.get_position_instrument()
         timeframe = Timeframe(strategy.get_timeframe())
         historical_data = self.historical_data_repository.get_historical_data(instrument, start_date, end_date, timeframe)
